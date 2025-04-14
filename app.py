31d29a21cb6d6e668e34bf371733ee26e4234746
besacier <6469915+besacier@users.noreply.github.com>
2025-04-14 14:34:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -14,3 +15,7 @@ def filter_data():
     except Exception as e:
         print(f"❌ Error: {e}")
         return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
